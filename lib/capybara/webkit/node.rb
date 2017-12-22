149ca290050aa6ae03d1d9a2d01cdbfd4bae1dcb
Mike Dalpee <mikedalpee@gmail.com>
2017-12-22 11:09:04 -0500

    Add attributes retrieval

--- lib/capybara/webkit/node.rb
+++ lib/capybara/webkit/node.rb
@@ -24,7 +24,7 @@ module Capybara::Webkit
         val = invoke(:attribute, name) && invoke(:property, name)
       else
         val = invoke(:property, name)
-        val = invoke(:attribute, name) if val.nil? || val.is_a?(Hash)
+        val = invoke(:attribute, name) if val.nil? || (val.is_a?(Hash) && name != "attributes")
       end
       val
     end
